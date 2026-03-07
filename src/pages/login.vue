ac2be2cc21e852825626c3656729bbdba32f96e4
Shah Sawood <shahsawood@my.uopeople.edu>
2026-03-07 16:08:46 +0500

    Revert "Revert "second commit""

---
+++ src/pages/login.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Sajjad khan </h1>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/soflow-clone',
   plugins: [
     vue(),
-    // vueDevTools(),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
