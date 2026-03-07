52423f14c2b58380f6e8c1dbf32b3e1be1476e8d
Shah Sawood <shahsawood@my.uopeople.edu>
2026-03-07 15:07:32 +0500

    Revert "second commit"

---
+++ src/components/input/InputField.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="input-group">
+    <label>{{ label }}</label>
+    <div class="input-wrapper">
+      <input
+        class="input-field"
+        type="text"
+        :placeholder="placeholder"
+        :value="modelValue"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    placeholder: String,
+    modelValue: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    onInput(e) {
+      this.$emit("update:modelValue", e.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-group {
+  margin-bottom: 20px;
+  text-align: left;
+}
+
+label {
+  display: block;
+  margin-bottom: 8px;
+  font-weight: 600;
+  color: #444;
+}
+
+.input-wrapper {
+  position: relative;
+}
+
+.input-field {
+  width: 100%;
+  padding: 14px;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  background: #e9edf3;
+  font-size: 14px;
+}
+
+.eye {
+  position: absolute;
+  right: 15px;
+  top: 14px;
+  cursor: pointer;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import home from '../pages/home.vue'
-import login from '../pages/home.vue'
+import Login from '../pages/auth/login.vue'
+import ResetPassword from '../pages/auth/reset.vue'
+import Dashboard from '../pages/dashboard/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: home,
+      name: 'login',
+      component: Login,
     },
     {
-      path: '/login',
-      name: 'login',
-      component: login,
+      path: '/reset-password',
+      name: 'reset-password',
+      component: ResetPassword,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     },
   ],
 })
 
 export default router
-
-
-
